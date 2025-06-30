c8e9716f771d9d7e04216d6433798f5bb8cf7513
dimpy-iroid <dimpypatel01296@gmail.com>
2025-06-30 11:33:12 +0530

    Initial Commit

---
+++ DemoCustomTabBarSwiftUI/DemoCustomTabBarSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoCustomTabBarSwiftUIApp.swift
+//  DemoCustomTabBarSwiftUI
+//
+//  Created by Dimpy Patel on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoCustomTabBarSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
